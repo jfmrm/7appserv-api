3518d60b6451b912ae210bfdbf4f35a78a2d57ba
João Filipe Moura <joaoflp1@Joaos-MBP.home>
2018-01-17 21:47:22 -0300

    Adds User super class, witch is instanciated by Pro and Costumer

---
+++ app/models/User.js
@@ -0,0 +1,9 @@
+export class User  {
+  constructor (firstName, lastName, email, password, contactNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.contactNumber = contactNumber;
+  }
+}
