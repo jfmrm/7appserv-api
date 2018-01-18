a25646a988c4baf826bc080c4c07769d6236b6db
João Filipe Moura <joaoflp1@Joaos-MBP.home>
2018-01-18 17:47:52 -0300

    Adds tests for Demand CRUD

--- populate.js
+++ populate.js
@@ -1,4 +1,5 @@
-import { Costumer, Pro, ProVIP, Place, Address } from './app/models';
+import { Costumer, Pro, ProVIP, Place, Address, Demand, ServiceType } from './app/models';
+import moment from 'moment';
 
 export function populate () {
 //costumer stuff
@@ -25,7 +26,7 @@ export function populate () {
 //---------------------------------------------------------------------------------------
 //address and place stuff
 
-  let address = new Address(null, 20, 'street', 'unit', 'city', 'st', 12345);
+  // let address = new Address(null, 20, 'street', 'unit', 'city', 'st', 12345);
   // address.create()
   //   .then((address) => {
   //     console.log(address)
@@ -54,7 +55,7 @@ export function populate () {
 //-----------------------------------------------------------------------------------
 //Pro stuff
 
-  let pro = new Pro('pedro', 'Moura', 'pedro@moura.com', '123', '+1123456789', address , true, null, null, null);
+  // let pro = new Pro('pedro', 'Moura', 'pedro@moura.com', '123', '+1123456789', address , true, null, null, null);
   // pro.create()
   //   .then((pro) => {
   //     console.log(pro)
@@ -67,20 +68,45 @@ export function populate () {
   //     console.log(result)
   //   });
 
-  pro.create()
-    .then((pro) => {
-      console.log(pro)
-      return new ProVIP(pro, '123456789', 'companyName').create();
-    }).then((proVIP) => {
-      console.log(proVIP)
-      proVIP.companyName = 'myCompany'
-      return proVIP.update()
-    }).then((proVIP) => {
-      console.log(proVIP)
-      return proVIP.remove()
-    }).then((res) => {
-      console.log(res)
-      return proVIP.remove()
-    });
+  // pro.create()
+  //   .then((pro) => {
+  //     console.log(pro)
+  //     return new ProVIP(pro, '123456789', 'companyName').create();
+  //   }).then((proVIP) => {
+  //     console.log(proVIP)
+  //     proVIP.companyName = 'myCompany'
+  //     return proVIP.update()
+  //   }).then((proVIP) => {
+  //     console.log(proVIP)
+  //     return proVIP.remove()
+  //   }).then((res) => {
+  //     console.log(res)
+  //     return proVIP.remove()
+  //   });
+
+// ----------------------------------------------------------------------------------------
+//Demand stuff
+  // let serviceType = new ServiceType(null, 'house cleaning');
+  // let costumer = new Costumer('joao', 'moura', 'joao@filipe.com', '123', '+1123456789');
+  // let address = new Address(null, 20, 'street', 'unit', 'city', 'st', 12345);
+  // let place = new Place(1, 1, 2, address);
+  //
+  // Promise.all([serviceType.create(), costumer.create(), place.create()])
+  //   .then((dependencies) => {
+  //     console.log(dependencies)
+  //     let demand = new Demand(null, dependencies[1], dependencies[2], dependencies[0], moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'), 'details', true).create()
+  //     return { demand: demand, dependencies: dependencies }
+  //   }).then(({ demand: demand, dependencies: dependencies }) => {
+  //     demand.then((demand) => {
+  //       console.log(demand)
+  //       return Promise.all(
+  //         [dependencies[0].remove(),
+  //          dependencies[1].remove(),
+  //          dependencies[2].remove(),
+  //          demand.remove()])
+  //     }).then((res) => {
+  //       console.log(res)
+  //     })
+  //   });
 
 }
