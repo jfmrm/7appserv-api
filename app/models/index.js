a2e8fd7d917b023a0251234460a1915146b068fc
João Filipe Moura <joaoflp1@Joaos-MBP.home>
2018-01-17 21:47:53 -0300

    Adds index file that exports all models

---
+++ app/models/index.js
@@ -0,0 +1,6 @@
+export { User }     from './User';
+export { Pro }      from './Pro';
+export { ProVIP }   from './ProVIP';
+export { Costumer } from './Costumer';
+export { Address }  from './Address';
+export { Place }    from './Place';
