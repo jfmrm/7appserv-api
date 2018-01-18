a36edd5ad09d1e18172356a8e8aaed2fbf6f7ce2
João Filipe Moura <joaoflp1@Joaos-MBP.home>
2018-01-17 21:43:37 -0300

    Adds Address model with CRUD

---
+++ app/models/Address.js
@@ -0,0 +1,47 @@
+import { Pool } from '../../config';
+
+export class Address {
+  constructor(addressId, numPlace, street, unit, city, state, zipCode) {
+    this.id = addressId;
+    this.numPlace = numPlace;
+    this.street = street;
+    this.unit = unit;
+    this.city = city;
+    this.state = state;
+    this.zipCode = zipCode;
+  }
+
+  create() {
+    return Pool.query('INSERT INTO address (num_place, street, unit, city, state, zip_code) VALUES (?, ?, ?, ?, ?, ?)',
+      [this.numPlace, this.street, this.unit, this.city, this.state, this.zipCode])
+        .then((results) => {
+          return this.get('id', results.insertId);
+        });
+  }
+
+  get(column, param) {
+    return Pool.query('SELECT * FROM address WHERE ' + column + ' = ?', [param])
+      .then((results) => {
+        return new Address(results[0].id, results[0].num_place, results[0].street, results[0].unit, results[0].city, results[0].state, results[0].zip_code)
+      });
+  }
+
+  update() {
+    return Pool.query('UPDATE address SET num_place = ?, street = ?, unit = ?, city = ?, state = ?, zip_code = ? WHERE id = ?',
+      [this.numPlace, this.street, this.unit, this.city, this.state, this.zipCode, this.id])
+        .then((results) => {
+          return this.get('id', this.id);
+        });
+  }
+
+  remove() {
+    return Pool.query('DELETE FROM address WHERE id = ?', [this.id])
+      .then((results) => {
+        if(results.affectedRows == 1) {
+          return true
+        } else {
+          return false
+        }
+      })
+  }
+}
