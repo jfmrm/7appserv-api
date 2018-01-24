11a7ab83cf2f3e98be8b4592ad2e1296ff5395ad
João Filipe Moura <joaoflp1@Joaos-MBP.home>
2018-01-24 09:46:41 -0300

    Alter update function to update address along with Pro

--- app/models/Pro.js
+++ app/models/Pro.js
@@ -36,10 +36,12 @@ export class Pro extends User {
   }
 
   update() {
-    return Pool.query('UPDATE pro SET email = ?, password = ?, first_name = ?, last_name = ?, average_response_time = ?, rate = ?, last_payment_date = ?, pro_type = ?, has_insurance = ?, contact_number = ?',
-    [this.email, this.password, this.firstName, this.lastName, this.avarageResponseTime, this.rate, this.lastPayment, 'Standard', this.hasInsurance, this.contactNumber])
+    let pro = Pool.query('UPDATE pro SET first_name = ?, last_name = ?, has_insurance = ?, contact_number = ? WHERE email = ?',
+    [this.firstName, this.lastName, this.hasInsurance, this.contactNumber, this.email])
+    let address = this.address.update()
+    return Promise.all([pro, address])
       .then((result) => {
-        return this.get('id', this.id)
+        return this.get('email', this.email)
       });
   }
 
