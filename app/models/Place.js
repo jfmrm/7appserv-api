8ba3c1bbcdabad25b015cb71ae67864f2997eecd
João Filipe Moura <jfmrm@cin.ufpe.br>
2018-01-25 08:11:40 -0300

    Fixes Place get method

--- app/models/Place.js
+++ app/models/Place.js
@@ -26,12 +26,13 @@ export class Place {
   get(column, param) {
     return Pool.query('SELECT * FROM place WHERE ' + column + ' = ?', [param])
       .then((results) => {
-        let address = new Address().get('id', results[0].address_id)
-        return { address: address, results: results}
-      }).then(({ address: address, results: results}) => {
-        return address.then((address) => {
-          return new Place(results[0].costumer_id, results[0].size, results[0].bathrooms, address, results[0].id)
-        })
+        return new Place(results[0].costumer_id, results[0].size, results[0].bathrooms, results[0].address_id, results[0].id)
+      }).then((place) => {
+        return new Address().get('id', place.address)
+          .then((address) => {
+            place.address = address
+            return place
+          })
       });
   }
 
@@ -61,22 +62,13 @@ export class Place {
 
   getListByCostumerId(costumerId) {
     //queries all Places from a costumer
-    return Pool.query('SELECT * FROM place WHERE costumer_id = ?', [costumerId])
+    return Pool.query('SELECT id FROM place WHERE costumer_id = ?', [costumerId])
       .then((results) => {
-        //for each place queries its address, and mounts an array of address
-        let addresses = Promise.all(results.map(function(place){
-          new Address().getByPlaceOrProId(place.id)
-            .then((address) => {
-              return address
-            })
-          }))
-      return { addresses: addresses, results: results}
-    }).then(({ addresses: addresses, results: results}) => {
-        let places = []
-        for (let i = 0; i < results; i++) {
-          places.push(new Place(results[i].size, results[i].bathrooms, addresses[i]))
-        }
-        return places;
+        return Promise.all(results.map((place) => {
+          return this.get('id', place.id)
+        }))
+      }).then((places) => {
+        return places
       });
   }
 }
