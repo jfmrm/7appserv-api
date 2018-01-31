f6e7eee4c73570ddc80e8dc6b575a5b0f72526ee
João Filipe Moura <jfmrm@cin.ufpe.br>
2018-01-31 12:15:39 -0300

    Alters Service model to receive start and startTime parameters and a start function

--- app/models/Service.js
+++ app/models/Service.js
@@ -2,13 +2,15 @@ import { Pool } from '../../config';
 import { Demand, Pro } from './';
 
 export class Service extends Demand {
-  constructor(demand, pro, isGoing, isDone, doneTime, serviceId) {
+  constructor(demand, pro, isGoing, isDone, doneTime, started, startTime, serviceId) {
     super(demand.id, demand.costumer, demand.place, demand.serviceType, demand.dueDate, demand.details, demand.isOpen, demand.lastModified);
     this.pro = pro;
     this.isGoing = isGoing;
     this.isDone = isDone;
     this.doneTime = doneTime;
     this.serviceId = serviceId;
+    this.started = started;
+    this.startTime = startTime;
   }
 
   create() {
@@ -27,7 +29,8 @@ export class Service extends Demand {
         return { results: results, p: p }
       }).then(({ results: results, p: p }) => {
         return p.then((res) => {
-          return new Service(res[0], res[1], results[0].is_going, results[0].is_done, results[0].done_time, results[0].id)
+          //not tested yet
+          return new Service(res[0], res[1], results[0].is_going, results[0].is_done, results[0].done_time, results[0].started, results[0].start_time, results[0].id)
         })
       }).catch((error) => {
         throw error
@@ -35,7 +38,8 @@ export class Service extends Demand {
   }
 
   update() {
-    return Pool.query('UPDATE service SET demand_id = ?, pro_id = ?, is_going = ?, isDone = ?, done_time = ?')
+    //not tested yet
+    return Pool.query('UPDATE service SET demand_id = ?, pro_id = ?', [this.demand_id, this.pro_id])
       .then((results) => {
         return this.get('id', this.serviceId)
       }).catch((error) => {
@@ -51,6 +55,18 @@ export class Service extends Demand {
         } else {
           throw new Error('Service does not exist')
         }
+      }).catch((error) => {
+        throw error
+      });
+  }
+
+//not tested yet
+  startService() {
+    return Pool.query('UPDATE service SET started = ? WHERE id = ?', [true, this.serviceId])
+      .then((restults) => {
+        return Pool.query('SELECT start_time FROM service WHERE id = ?', [this.serviceId])
+      }).then((restults) => {
+        return results[0].start_time
       }).catch((error) => {
         throw error
       });
