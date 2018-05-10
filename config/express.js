a089e91c20c1c4bf55846c9b27b4112c134ed689
João Filipe Moura <jfmrm@cin.ufpe.br>
2018-05-10 17:18:54 -0300

    Some stuff

--- config/express.js
+++ config/express.js
@@ -17,7 +17,7 @@ import { CustomerUserRouter,
          CityRouter } from 'routes';
 import { getPro,
          getDemand } from './middlewares';
-
+         
 let awsConfig = new aws.Config({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
